a9afcebdace90ee788e38d93eb7e13801b66b8b0
Vitu <victor.roberto@al.resilia.com.br>
2020-04-05 16:14:32 -0300

    second commit

---
+++ csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+       static void Main(){
+
+            var pilha = new pilha();
+            pilha.Empilha(1);
+            pilha.Empilha(2);
+            pilha.Empilha(3);
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+       }
+    }
+}
+ 
